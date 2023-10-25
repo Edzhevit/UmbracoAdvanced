d33bc5bdc4ad3147c26a9ab145e2f132a345053a
Edzhevit <edzhevit.gyureev@endava.com>
2023-10-25 14:34:57 +0300

    Add models generated in core project, configure block list editor with custom views and settings

---
+++ UmbracoAdvanced.Core/Models/Umbraco/Home.cs
@@ -0,0 +1,7 @@
+﻿namespace UmbracoAdvanced.Core.Models.Umbraco;
+
+public partial class Home
+{
+    public string ShortHeroDescription => 
+        string.IsNullOrEmpty(HeroDescription) ? "" : $"{HeroDescription[..30]} ...";
+}
